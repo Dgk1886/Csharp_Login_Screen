093796ec3c58503c6916b38732083307d80c0bb3
Emir Doğukan Kılıç <kilicemirdogukan@gmail.com>
2022-08-22 10:07:06 +0300

    There is the codes

---
+++ LoginPaneli/LoginPaneli/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LoginPaneli
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginbutton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.Pwsee = new System.Windows.Forms.Label();
+            this.ErrorLabel = new System.Windows.Forms.Label();
+            this.ForgotPw = new System.Windows.Forms.Label();
+            this.signupbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginbutton
+            // 
+            this.loginbutton.Location = new System.Drawing.Point(157, 150);
+            this.loginbutton.Name = "loginbutton";
+            this.loginbutton.Size = new System.Drawing.Size(92, 31);
+            this.loginbutton.TabIndex = 0;
+            this.loginbutton.Text = "Giriş Yap";
+            this.loginbutton.UseVisualStyleBackColor = true;
+            this.loginbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Şifre:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(336, 153);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Beni Hatırla";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Pwsee
+            // 
+            this.Pwsee.AutoSize = true;
+            this.Pwsee.Location = new System.Drawing.Point(320, 97);
+            this.Pwsee.Name = "Pwsee";
+            this.Pwsee.Size = new System.Drawing.Size(85, 16);
+            this.Pwsee.TabIndex = 6;
+            this.Pwsee.Text = "Şİfreyi göster";
+            this.Pwsee.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // ErrorLabel
+            // 
+            this.ErrorLabel.AutoSize = true;
+            this.ErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorLabel.Location = new System.Drawing.Point(242, 69);
+            this.ErrorLabel.Name = "ErrorLabel";
+            this.ErrorLabel.Size = new System.Drawing.Size(36, 16);
+            this.ErrorLabel.TabIndex = 7;
+            this.ErrorLabel.Text = "Error";
+            this.ErrorLabel.Visible = false;
+            // 
+            // ForgotPw
+            // 
+            this.ForgotPw.AutoSize = true;
+            this.ForgotPw.ForeColor = System.Drawing.Color.Blue;
+            this.ForgotPw.Location = new System.Drawing.Point(217, 119);
+            this.ForgotPw.Name = "ForgotPw";
+            this.ForgotPw.Size = new System.Drawing.Size(99, 16);
+            this.ForgotPw.TabIndex = 8;
+            this.ForgotPw.Text = "Şifremi Unuttum";
+            this.ForgotPw.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // signupbutton
+            // 
+            this.signupbutton.Location = new System.Drawing.Point(255, 150);
+            this.signupbutton.Name = "signupbutton";
+            this.signupbutton.Size = new System.Drawing.Size(75, 31);
+            this.signupbutton.TabIndex = 9;
+            this.signupbutton.Text = "Kaydol";
+            this.signupbutton.UseVisualStyleBackColor = true;
+            this.signupbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(514, 213);
+            this.Controls.Add(this.signupbutton);
+            this.Controls.Add(this.ForgotPw);
+            this.Controls.Add(this.ErrorLabel);
+            this.Controls.Add(this.Pwsee);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginbutton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loginbutton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label Pwsee;
+        private System.Windows.Forms.Label ErrorLabel;
+        private System.Windows.Forms.Label ForgotPw;
+        private System.Windows.Forms.Button signupbutton;
+    }
+}
+
---
+++ LoginPaneli/LoginPaneli/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginPaneli
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            bool durum = textBox2.UseSystemPasswordChar;
+            if (durum)
+            {
+                textBox2.UseSystemPasswordChar = false;
+                Pwsee.Text = "Şifreyi Gösterme";
+            }
+            else
+            {
+                textBox2.UseSystemPasswordChar = true;
+                Pwsee.Text = "Şifreyi göster";
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Length < 9 || textBox1.Text.Length > 16)
+            {
+                ErrorLabel.Text = "Kullanıcı adı 9 ila 16 karakterden oluşabilir";
+                ErrorLabel.Visible = true;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mail adresinize sıfırlama kodu gönderilmiştir");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 fr = new Form2();
+            fr.Show();
+            this.Hide();
+        }
+    }
+}
